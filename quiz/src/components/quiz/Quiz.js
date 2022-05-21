59d02d56d4037fa73eea25643ea46ea1297511e3
abhayp2204 <abhaypatil1020@gmail.com>
2022-05-22 00:35:20 +0530

    questions

--- quiz/src/components/quiz/Quiz.js
+++ quiz/src/components/quiz/Quiz.js
@@ -1,10 +1,11 @@
 import React, { useState, useContext } from "react"
-import { quizListContext } from "../App"
+import { query, quizListContext } from "../App"
 import { useParams } from "react-router-dom"
 import "../../css/Question.css"
 
 import "firebase/compat/firestore"
 import "firebase/compat/auth"
+import firebase from "firebase/compat/app"
 import { firestore } from "../../firebase"
 import { useCollectionData } from "react-firebase-hooks/firestore"
 
@@ -12,6 +13,8 @@ function Quiz(props) {
     const quizList = useContext(quizListContext)
     const { id } = useParams()
     const [newQuestion, setNewQuestion] = useState("")
+    var db = firebase.firestore()
+    const quizzesRef = firestore.collection("quizzes")
     
     if(!quizList) return
 
@@ -23,7 +26,22 @@ function Quiz(props) {
             return
         }
 
-        console.log(Q.questions)
+        query.get().then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                if(doc.data().id === id) {
+                    doc.ref.delete()
+                }
+            })
+        })
+
+        await quizzesRef.add({
+            name: Q.name,
+            id: Q.id,
+            createdAt: Q.createdAt,
+            questions: Q.questions,
+            uid: Q.uid, 
+        })
+
         setNewQuestion("")
     }
 
@@ -35,10 +53,16 @@ function Quiz(props) {
         }
     })
 
+    // Array of questions
+    var questionList = []
+    Q.questions && Q.questions.map(question => {
+        questionList.push(<p>{question}</p>)
+    })
 
     return (
         <div className="question-container">
             <p className="quiz-title">{Q.name}</p>
+            <p>{questionList}</p>
             <form onSubmit={handleAddQuestion}>
                 <input
                     className="add-question-input pop"
